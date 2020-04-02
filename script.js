0c81a0a9f023f31b17689438be9bbaf906ac4437
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-04-01 18:23:12 -0600

    completed pulling in the data for the modal section and refined some styles

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function displayCountries(countries) {
             </div>
 
             <div class="card-body">
-                <h2 class="country-name">${country.name}</h2>
+                <h3 class="country-name">${country.name}</h3>
                 <p>
                     <strong>Population: </strong>
                     ${country.population}
@@ -53,10 +53,55 @@ function displayCountries(countries) {
 
         countryEl.addEventListener('click', () => {
             modal.style.display = 'flex';
+            showCountryDetails(country);
         });
 
         countriesEl.appendChild(countryEl);
     });
+}
+
+// pull additional country data to show in modal
+function showCountryDetails(country) {
+    const modalBody = modal.querySelector('.modal-body');
+    const modalImg = modal.querySelector('img');
+
+        modalImg.src = country.flag;
+        modalBody.innerHTML = `
+        <h2>${country.name}</h2>
+        <p>
+            <strong>Native Name: </strong>
+            ${country.nativeName}
+        </p>
+        <p>
+            <strong>Population: </strong>
+            ${country.population}
+        </p>
+        <p>
+            <strong>Region: </strong>
+            ${country.region}
+        </p>
+        <p>
+            <strong>Sub Region: </strong>
+            ${country.subregion}
+        </p>
+        <p>
+            <strong>Capital: </strong>
+            ${country.capital}
+        </p>
+        <p>
+            <strong>Top Level Domain: </strong>
+            ${country.topLevelDomain[0]}
+        </p>
+        <p>
+            <strong>Currencies: </strong>
+            ${country.currencies.map(currency => currency.name)}
+        </p>
+        <p>
+            <strong>Languages: </strong>
+            ${country.languages.map(language => language.name)}
+        </p>
+        
+    `;
 }
 
 // toggle theme event listener
