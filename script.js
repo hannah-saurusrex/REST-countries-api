6bcd9bf62b0a57d56e022b1e084c8e8d7975ea61
hannah-saurusrex <hrose.mcmahon@gmail.com>
2020-04-10 16:49:43 -0600

    completed eslint and prettier config

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// Todos
-// API call ✅
-// theme toggle ✅
-// search ✅
-// filter ✅
-// modal ✅
-
 const countriesEl = document.getElementById('countries');
 const toggleBtn = document.getElementById('toggle');
 const filterBtn = document.getElementById('filter');
@@ -13,23 +6,14 @@ const searchEl = document.getElementById('search');
 const modal = document.getElementById('modal');
 const closeBtn = document.getElementById('close');
 
-getCountries();
-
-async function getCountries() {
-    const res = await fetch('https://restcountries.eu/rest/v2/all');
-    const countries = await res.json();
-
-    displayCountries(countries);
-}
-
 function displayCountries(countries) {
-    countriesEl.innerHTML = '';
+  countriesEl.innerHTML = '';
 
-    countries.forEach(country => {
-        const countryEl = document.createElement('div');
-        countryEl.classList.add('card');
-    
-        countryEl.innerHTML = `
+  countries.forEach(country => {
+    const countryEl = document.createElement('div');
+    countryEl.classList.add('card');
+
+    countryEl.innerHTML = `
             <div>
                 <img src="${country.flag}" alt="${country.name}">
             </div>
@@ -38,10 +22,11 @@ function displayCountries(countries) {
                 <h3 class="country-name">${country.name}</h3>
                 <p>
                     <strong>Population:</strong>
-                    ${country.population
+                    ${
+                      country.population
                         .toString()
-                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",") 
-                        // format number as thousands, with comma seperator. Thank you Stack Overflow (https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript)
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                      // format number as thousands, with comma seperator. Thank you Stack Overflow (https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript)
                     }
                 </p>
                 <p class="country-region">
@@ -55,22 +40,31 @@ function displayCountries(countries) {
             </div>
         `;
 
-        countryEl.addEventListener('click', () => {
-            modal.style.display = 'flex';
-            showCountryDetails(country);
-        });
-
-        countriesEl.appendChild(countryEl);
+    countryEl.addEventListener('click', () => {
+      modal.style.display = 'flex';
+      showCountryDetails(country);
     });
+
+    countriesEl.appendChild(countryEl);
+  });
 }
+
+async function getCountries() {
+  const res = await fetch('https://restcountries.eu/rest/v2/all');
+  const countries = await res.json();
+
+  displayCountries(countries);
+}
+
+getCountries();
 
 // pull additional country data to show in modal
 function showCountryDetails(country) {
-    const modalBody = modal.querySelector('.modal-body');
-    const modalImg = modal.querySelector('img');
+  const modalBody = modal.querySelector('.modal-body');
+  const modalImg = modal.querySelector('img');
 
-        modalImg.src = country.flag;
-        modalBody.innerHTML = `
+  modalImg.src = country.flag;
+  modalBody.innerHTML = `
         <h2>${country.name}</h2>
         <p>
             <strong>Native Name: </strong>
@@ -78,10 +72,11 @@ function showCountryDetails(country) {
         </p>
         <p>
             <strong>Population: </strong>
-            ${country.population
+            ${
+              country.population
                 .toString()
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ",") 
-                // format number as thousands, with comma seperator. Thank you Stack Overflow
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+              // format number as thousands, with comma seperator. Thank you Stack Overflow
             }
         </p>
         <p>
@@ -102,17 +97,11 @@ function showCountryDetails(country) {
         </p>
         <p>
             <strong>Currencies: </strong>
-            ${country.currencies
-                .map(currency => currency.name)
-                .join(', ')
-            }
+            ${country.currencies.map(currency => currency.name).join(', ')}
         </p>
         <p>
             <strong>Languages: </strong>
-            ${country.languages
-                .map(language => language.name)
-                .join(', ')
-            }
+            ${country.languages.map(language => language.name).join(', ')}
         </p>
         
     `;
@@ -120,48 +109,48 @@ function showCountryDetails(country) {
 
 // toggle theme event listener
 toggleBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
+  document.body.classList.toggle('dark');
 });
 
 // show & hide the filter options list
 filterBtn.addEventListener('click', () => {
-    filterBtn.classList.toggle('open');
+  filterBtn.classList.toggle('open');
 });
 
 // close the modal
 closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
+  modal.style.display = 'none';
 });
 
 // search input function
-searchEl.addEventListener('input', (e) => {
-    const { value } = e.target
-    const countryName = document.querySelectorAll('.country-name');
+searchEl.addEventListener('input', e => {
+  const { value } = e.target;
+  const countryName = document.querySelectorAll('.country-name');
 
-    countryName.forEach(name => {
-        if(name.innerText.toLowerCase().includes(value.toLowerCase())) {
-            name.parentElement.parentElement.style.display = 'block'; 
-            // .country-name -> .card-body -> .card
-            // go up two levels to the countryEl.innerHTML; if it matches, display
-        } else {
-            name.parentElement.parentElement.style.display = 'none';
-        }
-    });
+  countryName.forEach(name => {
+    if (name.innerText.toLowerCase().includes(value.toLowerCase())) {
+      name.parentElement.parentElement.style.display = 'block';
+      // .country-name -> .card-body -> .card
+      // go up two levels to the countryEl.innerHTML; if it matches, display
+    } else {
+      name.parentElement.parentElement.style.display = 'none';
+    }
+  });
 });
 
 // search countries by region; or, add a filter to the 'li's inside the .dropdown
 regionFilters.forEach(filter => {
-    filter.addEventListener('click', () => {
-        const value = filter.innerText;
-        const countryRegion = document.querySelectorAll('.country-region');
+  filter.addEventListener('click', () => {
+    const value = filter.innerText;
+    const countryRegion = document.querySelectorAll('.country-region');
 
-        countryRegion.forEach(region => {
-            if(region.innerText.includes(value) || value === 'All') {
-                region.parentElement.parentElement.style.display = 'block'; 
-                // .country-region -> .card-body -> .card
-            } else {
-                region.parentElement.parentElement.style.display = 'none';
-            }
-        });
+    countryRegion.forEach(region => {
+      if (region.innerText.includes(value) || value === 'All') {
+        region.parentElement.parentElement.style.display = 'block';
+        // .country-region -> .card-body -> .card
+      } else {
+        region.parentElement.parentElement.style.display = 'none';
+      }
     });
+  });
 });
